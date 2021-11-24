13746b5054cea45e05304f866ee7468ce8252b06
Ananias Raphael <ananias.nobrega@gmail.com>
2021-11-24 12:16:48 -0300

    fix

---
+++ src/main/java/com/ananias/examplelogin/ExampleLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ananias.examplelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ananias/examplelogin/ExampleLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ananias.examplelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
